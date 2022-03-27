a3f0a8217b112966e59d12df8af3edf8a1e7dc07
OwenDa <owendarcy25@gmail.com>
2022-03-27 10:33:21 +0000

    Import scipy stats and sketch out t-test functionality

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from scipy import stats
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-# Write your code to expect a terminal of 80 characters wide and 24 rows high.
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -16,5 +16,25 @@ SHEET = GSPREAD_CLIENT.open('pp3')
 data = SHEET.worksheet('data')
 
 past_data = data.get_all_values()
+print(past_data)  # Testing sheet
 
-print(past_data)
+sample_a = [66.1, 69.9, 67.7, 69.6, 71.1]  # Test data
+sample_b = [83.7, 81.5, 80.6, 83.9, 84.4]  # Test data
+
+result = stats.ttest_ind(sample_a, sample_b)
+
+ALPHA = 0.05  # significance level
+
+
+def output_result():
+    """
+    Outputs result of t-test to terminal in terms of significance
+    """
+    if result[1] < ALPHA:
+        print("significance")
+    else:
+        print("non stat-sig")
+
+
+output_result()
+# Reminder: Expect a terminal of 80 characters wide and 24 rows high.
